45df4b6da0ea4efed2fe62f29041c0c0250d44ca
Alexander <potemkin_av7@mail.ru>
2025-08-01 12:09:55 +0300

    Initial Commit

---
+++ DailyQuiz/DailyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuizApp.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
