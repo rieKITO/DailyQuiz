e53b93a26ad36314ae368cc3521d3e3b5c1c9e8e
Alexander <potemkin_av7@mail.ru>
2025-08-01 15:29:26 +0300

    [Feature] Add QuizQuestion model

---
+++ DailyQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,37 @@
+//
+//  Question.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 01.08.2025.
+//
+
+import Foundation
+
+struct QuizResponse: Codable {
+    let responseCode: Int
+    let results: [QuizQuestion]
+
+    enum CodingKeys: String, CodingKey {
+        case responseCode = "response_code"
+        case results
+    }
+}
+
+struct QuizQuestion: Codable, Identifiable {
+    var id: UUID = UUID()
+    let type: String
+    let difficulty: String
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case type
+        case difficulty
+        case category
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
+}
