5053fff638e0c984c25e3bdb43d7f87cc05b3102
Alexander <potemkin_av7@mail.ru>
2025-08-01 12:26:28 +0300

    [Feature] Add HomeView

---
+++ DailyQuiz/Core/Home/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 01.08.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
