c7b0697134a72ac38d45876c5596606d64e50ed2
Alexander <potemkin_av7@mail.ru>
2025-08-01 13:56:54 +0300

    [Feature] Add logo and history button in HomeView

--- DailyQuiz/Core/Home/Views/HomeView.swift
+++ DailyQuiz/Core/Home/Views/HomeView.swift
@@ -8,10 +8,62 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //background
+            Color.appThemeColors.moodyBlue.edgesIgnoringSafeArea(.all)
+            
+            // foreground
+            VStack {
+                historyButton
+                logo
+                VStack {
+                    Text("Добро пожаловать в DailyQuiz!")
+                        .foregroundStyle(Color.appThemeColors.accent)
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundStyle(Color.appThemeColors.white)
+                )
+            }
+        }
     }
 }
+
+// MARK: - Layout
+
+private extension HomeView {
+    
+    private var historyButton: some View {
+        HStack(spacing: 20) {
+            Text("История")
+                .foregroundStyle(Color.appThemeColors.moodyBlue)
+                .font(.headline)
+            Image("history")
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundStyle(Color.appThemeColors.white)
+        )
+    }
+    
+    private var logo: some View {
+        Image("logo")
+            .resizable()
+            .frame(width: 300, height: 68)
+    }
+    
+}
+
+// MARK: - Preview
 
 #Preview {
     HomeView()
