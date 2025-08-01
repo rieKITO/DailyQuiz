2237a5a8467b682cea2897c224e35678ae85684e
Alexander <potemkin_av7@mail.ru>
2025-08-01 15:51:22 +0300

    [Feature] Add DeveloperPreview class

---
+++ DailyQuiz/Utilies/DeveloperPreview.swift
@@ -0,0 +1,16 @@
+//
+//  DeveloperPreview.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 01.08.2025.
+//
+
+import Foundation
+
+final class DeveloperPreview {
+    
+    static let shared = DeveloperPreview()
+    
+    let quizViewModel = QuizViewModel()
+    
+}
