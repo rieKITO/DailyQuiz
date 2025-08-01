9654e0e536c43250a02b2346d7ac53c9edf01362
Alexander <potemkin_av7@mail.ru>
2025-08-01 14:19:46 +0300

    [Feature] Add rounded rectangle button component

---
+++ DailyQuiz/Core/Components/RoundedRectangleButton.swift
@@ -0,0 +1,44 @@
+//
+//  RoundedRectangleButton.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 01.08.2025.
+//
+
+import SwiftUI
+
+struct RoundedRectangleButton: View {
+    
+    // MARK: - Properties
+    
+    let text: String
+    
+    let textColor: Color
+    
+    let backgroundColor: Color
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Text(text)
+            .foregroundStyle(textColor)
+            .font(.headline)
+            .fontWeight(.heavy)
+            .padding()
+            .padding(.horizontal, 30)
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(backgroundColor)
+            )
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    RoundedRectangleButton(
+        text: "НАЧАТЬ ВИКТОРИНУ",
+        textColor: Color.appThemeColors.white,
+        backgroundColor: Color.appThemeColors.moodyBlue
+    )
+}
