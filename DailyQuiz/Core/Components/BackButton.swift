2da41ea44c12273ad82cdd9c8e707519a119fd2f
Alexander <potemkin_av7@mail.ru>
2025-08-02 18:34:20 +0300

    [Feature] Add custom BackButton component

---
+++ DailyQuiz/Core/Components/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 02.08.2025.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
