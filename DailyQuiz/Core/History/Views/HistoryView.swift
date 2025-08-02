83c8dd28b7a41fe22214a54e398f9fc9a350cf61
Alexander <potemkin_av7@mail.ru>
2025-08-02 22:12:32 +0300

    [Patch] Remove testing Text

--- DailyQuiz/Core/History/Views/HistoryView.swift
+++ DailyQuiz/Core/History/Views/HistoryView.swift
@@ -24,7 +24,7 @@ struct HistoryView: View {
             // foreground
             VStack {
                 header
-                Text("\(historyViewModel.quizHistory[0].correctAnswersCount)")
+                
                 Spacer()
             }
         }
