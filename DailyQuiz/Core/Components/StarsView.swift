c738fabd2dc5c2ec74ced9885466d6a993400f8a
Alexander <potemkin_av7@mail.ru>
2025-08-02 14:23:44 +0300

    [Feature] Add star_fill image. Add StarsView

---
+++ DailyQuiz/Core/Components/StarsView.swift
@@ -0,0 +1,25 @@
+//
+//  StarsView.swift
+//  DailyQuiz
+//
+//  Created by Александр Потёмкин on 02.08.2025.
+//
+
+import SwiftUI
+
+struct StarsView: View {
+    
+    let rating: Int
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            ForEach(1..<6) { index in
+                Image(index <= rating ? "star_fill" : "star")
+            }
+        }
+    }
+}
+
+#Preview {
+    StarsView(rating: 2)
+}
